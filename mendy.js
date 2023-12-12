e9d975b8b538d93370aa1c081b307ae9d25b78c4
bright adams <btadamz@gmail.com>
2023-12-12 16:17:06 +0100

    Lettest

--- mendy.js
+++ mendy.js
@@ -38,5 +38,5 @@ if (answer3.toUpperCase() === correctAnswer3) {
 const percent = Math.round((correctAnswers / totalQuestions) * 100);
 
 console.log("You gott", correctAnswers, "questions correct!");
-console.log("You scored", percent.toString() + "%");
+console.log("You scorfed", percent.toString() + "%");
 
